ea28c481b9359036118f8e0d77b3a3104c1d4a36
cachemoney <cachebroker@gmail.com>
2012-11-26 21:58:59 -0600

    searches for a movie

---
+++ lib/api.rb
@@ -0,0 +1,22 @@
+require "open-uri"
+require "json"
+require "ostruct"
+require_relative "./movie"
+class Api
+
+	APIKEY = "ad7dvchc68qkrwyetu2h5rap"
+
+	def self.search_by_title(title)
+		url = "http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=#{APIKEY}&q=#{URI.encode(title)}&page_limit=1"
+		struct = OpenStruct.new(get_url_as_json(url).fetch("movies").first)
+		Movie.new(id: struct.id.to_i,
+							title: struct.title,
+							year: struct.year,
+							score: struct.ratings["critics_score"]
+							)
+	end
+
+	def self.get_url_as_json(url)
+		JSON.parse(open(url).read)
+	end
+end
---
+++ spec/movie_spec.rb
@@ -0,0 +1,17 @@
+require_relative "../lib/api"
+require_relative "../lib/movie"
+
+describe Movie do
+
+	it "should store the id, title, year, and score" do
+		movie = Movie.new(id: "the-id", title: "the-title", year: 1998, score: 50)
+		movie.id.should eq("the-id")
+		movie.title.should eq("the-title")
+		movie.year.should eq(1998)
+		movie.score.should eq(50)
+	end
+
+	it "should tell me the critic score" do
+
+	end
+end
